4996e435af680ccf49367e44ed24514a82dad49b
mkt-Do <mkt.koro.mikuru@gmail.com>
2019-10-23 11:05:25 +0900

    two sum

---
+++ golang/codewars/two_sum.go
@@ -0,0 +1,12 @@
+package codewars
+
+func TwoSum(numbers []int, target int) [2]int {
+	for i := 0; i < len(numbers)-1; i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i]+numbers[j] == target {
+				return [2]int{i, j}
+			}
+		}
+	}
+	return [2]int{}
+}
